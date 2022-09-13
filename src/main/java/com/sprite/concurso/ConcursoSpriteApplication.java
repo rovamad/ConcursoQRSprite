34f5470bd4a64665c0b560a024311204e562654e
rovamad <rojasmarcos@outlook.com>
2022-09-13 05:29:51 -0300

    Resolviendo info faltante para Deploy

--- src/main/java/com/sprite/concurso/ConcursoSpriteApplication.java
+++ src/main/java/com/sprite/concurso/ConcursoSpriteApplication.java
@@ -1,12 +1,17 @@
 package com.sprite.concurso;
 
-import com.sprite.concurso.service.JuegoService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ConcursoSpriteApplication {
+public class ConcursoSpriteApplication extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ConcursoSpriteApplication.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(ConcursoSpriteApplication.class, args);
 	}
